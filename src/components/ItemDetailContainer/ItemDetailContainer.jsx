cb60484c2dd4cfb1ed3b332369e12528b3614f57
guillermo-12345 <guillermo.ibanezc@gmail.com>
2024-11-25 15:04:59 -0300

    Corregir importación de ItemDetail y verificar estructura del proyecto

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
-import ItemDetail from '.../ItemDetail/ItemDetail'; 
+import ItemDetail from '../ItemDetail/ItemDetail'; 
 
 const ItemDetailContainer = () => {
   const { id } = useParams(); 
